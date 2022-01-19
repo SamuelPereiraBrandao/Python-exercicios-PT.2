7394a11e89109af10a9471d74df4a60890cd9913
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-19 19:44:21 -0300

    Add files via upload

---
+++ ex028.py
@@ -0,0 +1,2 @@
+tempo = int(input("Quantos anos tem seu carro"))
+print("carro novo" if tempo <= 3 else "carro velho")
---
+++ ex029.py
@@ -0,0 +1,6 @@
+nome = str(input("Qual seu nome?")).strip().upper()
+if nome == "SAMUEL":
+    print("Que legal, o meu também é SAMUEL!")
+else:
+    print("Infelizmente não temos o mesmo nome!")
+print("Tenha um bom dia, {}".format(nome))
